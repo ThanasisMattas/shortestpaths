4589653a0662d79138dabed0dae20ddff4e34b22
Thanasis Mattas <atmattas@physics.auth.gr>
2021-02-28 14:19:41 +0200

    feat: utils.check_nodal_connection()

--- pathplanning/utils.py
+++ pathplanning/utils.py
@@ -15,45 +15,80 @@ import ast
 from datetime import datetime, timedelta
 from itertools import combinations
 from functools import wraps
+from operator import itemgetter
 import random
 from timeit import default_timer as timer
+from typing import Iterable
 
 import click
 import matplotlib.pyplot as plt
 import networkx as nx
 
 
-def plot_graph(G, path, path_cost):
-  """Plots the graph in a spring_layout."""
-  shortest_path_edge_list = list(zip(path, path[1:]))
-
+def plot_graph(G, paths_data, disconnected_nodes):
+  """Plots the graph and all the generated paths in spring_layout."""
+  # pos = nx.spring_layout(G)
   pos = nx.spring_layout(G)
-  nx.draw_networkx(G, pos, node_size=450, width=0.3)
-  nx.draw_networkx_edges(G, pos=pos, edgelist=shortest_path_edge_list,
-                         edge_color='r', width=3)
-  plt.title(
-    f"#nodes: {G.number_of_nodes()}    #edges: {G.number_of_edges()}    "
-    f"path cost: {path_cost}\npath: {str(path)}"
-  )
-  plt.show()
 
+  # Layouts
+  # -------
+  # circular_layout
+  # spring_layout                <--
+  # fruchterman_reingold_layout  <--
+  # spiral_layout                <--
 
-def plot_aternative_paths(G, alternative_paths_list):
-  """Plots the graph and the alternative paths in spring_layout."""
-  pos = nx.spring_layout(G)
-  nx.draw_networkx(G, pos, node_size=450, width=0.3)
-  for path in alternative_paths_list:
-    shortest_path_edge_list = list(zip(path[0], path[0][1:]))
-    nx.draw_networkx_edges(G, pos=pos, edgelist=shortest_path_edge_list,
-                           edge_color='r', width=3)
-    # if path[2] is not None:
-    #   nx.draw_networkx_nodes(G, pos=pos, nodelist=path[2], node_color='r',
-    #                          node_shape='x', node_size=600, linewidths=2)
+  # 1. Draw the graph
+  nx.draw_networkx(G, pos, node_size=450, width=0.2, alpha=0.3,
+                   with_labels=False)
+  # 2. Draw the disconnected nodes
+  nx.draw_networkx_nodes(G, pos=pos, nodelist=disconnected_nodes, node_color='r',
+                         node_shape='x', node_size=800, linewidths=3)
 
-  # plt.title(
-  #   f"#nodes: {G.number_of_nodes()}    #edges: {G.number_of_edges()}    "
-  #   f"path cost: {path_cost}\npath: {str(path)}"
-  # )
+  # colors = iter(['b', 'm', 'g', 'k', 'r', 'c', 'y', 'w'])
+  colors = iter(['mediumblue', 'm', 'g', 'k', 'r', 'c', 'y', 'w'])
+  # Accumulates all the nodes of all the paths for label drawing.
+  paths_nodes = set()
+
+  for path in paths_data:
+    paths_nodes.update(path[0])
+
+  # Change the font of the labels of the path nodes and restore alpha=None.
+  for node, (x, y) in pos.items():
+    if node in paths_nodes:
+      plt.text(x, y, node, fontsize=14, ha='center', va='center')
+
+  # 3. Draw the nodes of all the paths
+  nx.draw_networkx_nodes(G, pos=pos, nodelist=paths_nodes, node_size=700,
+                         edgecolors='k', node_color="deepskyblue")
+
+  # 4. Draw the paths
+  for i, path in enumerate(paths_data):
+    color = next(colors)
+    path_edges_sequence = list(zip(path[0], path[0][1:]))
+
+    label = (f"path_{i + 1}: {str(path[0])}\ncost: {path[1]}    "
+             f"disconnected nodes: {str(path[2])}")
+
+    # Draw the path
+    nx.draw_networkx_edges(G, pos=pos, edgelist=path_edges_sequence,
+                           edge_color=color,
+                           width=len(paths_data) + 14 - 4 * i, label=label)
+
+    # Mark the disconnceted node with an X.
+    nx.draw_networkx_nodes(G, pos=pos, nodelist=path[2], node_color=color,
+                           node_shape='x', node_size=800, linewidths=3)
+
+  # Draw 'Start' & 'End' labels.
+  # labels = {paths_data[0][0][0]: "Start", paths_data[0][0][-1]: "End"}
+  # for node, (x, y) in pos.items():
+  #   if node in labels.keys():
+  #     plt.text(x + 50, y + 50, node, fontsize=14, ha='center', va='center')
+  # nx.draw_networkx_labels(G, pos=pos, labels=labels,
+  #                         font_color='k', font_size=20,
+  #                         bbox=dict(boxstyle="square", fc='w', ec='k'))
+
+  plt.title(f"#nodes: {G.number_of_nodes()}    #edges: {G.number_of_edges()}")
+  plt.legend()
   plt.show()
 
 
@@ -69,7 +104,7 @@ def _edge_weight_bias(edge, num_nodes) -> float:
   """
   # Bias will be capped with one of [0.1, 0.2, ..., 1.0], depending on with bin
   # it falls into.
-  bias_bins = [0.1 * i for i in range(1, 11)]
+  bias_bins = [0.1 * i**2 for i in range(1, 11)]
   bias = abs(edge[0] - edge[1]) / num_nodes
   for b in bias_bins:
     if bias < b:
@@ -107,6 +142,7 @@ def random_graph(num_nodes,
   """
   if random_seed is None:
     random_seed = datetime.now
+    # random_seed = 99
   random.seed(random_seed)
 
   nodes = list(range(1, num_nodes + 1))
@@ -140,7 +176,7 @@ def random_graph(num_nodes,
     # realistic - edges of nearby nodes cost less - and paths with too few
     # nodes, that go straight to the end, are avoided.)
     # Namely, distance (up) (down) edge_probability.
-    edge_probability = max(0, 1 - abs(edge[0] - edge[1]) / num_nodes - 0.6)
+    edge_probability = max(0, 1 - abs(edge[0] - edge[1]) / num_nodes - 0.5)
     random_probability = random.random()
     if edge_probability > random_probability:
       bias = _edge_weight_bias(edge, num_nodes)
@@ -197,3 +233,22 @@ class PythonLiteralOption(click.Option):
             return ast.literal_eval(value)
         except:
             raise click.BadParameter(value)
+
+
+def check_nodal_connection(nodes: Iterable,
+                           adj_list: list,
+                           disconnected_nodes: Iterable) -> Iterable:
+  """Checks for connection status of important nodes, i.e. start node and,
+  in case of a disconnected important node, it replaces it with the closest
+  neighbor.
+  """
+  for i, node in enumerate(nodes):
+    if node in disconnected_nodes:
+      input(f"Node <{node}> is disconnected. Setting it to its closest "
+            "first neighbor. Press ENTER to continue...")
+      node_neighbors = sorted(adj_list[node], key=itemgetter(1))
+      for neighbor in node_neighbors:
+        if neighbor[1] not in disconnected_nodes:
+          nodes[i] = neighbor[1]
+          break
+  return nodes
