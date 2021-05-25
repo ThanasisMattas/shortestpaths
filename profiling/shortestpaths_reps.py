9a3e555bd5005b7efa2882c0368a8e01e729e904
Thanasis Mattas <atmattas@physics.auth.gr>
2021-05-26 00:42:46 +0300

    chore: separate measure function: graphs are garbage collected

--- profiling/shortestpaths_reps.py
+++ profiling/shortestpaths_reps.py
@@ -24,6 +24,56 @@ import matplotlib.pyplot as plt
 from shortestpaths import core, graph_generator
 
 
+def measure(n,
+            i,
+            modes,
+            times,
+            failing,
+            online,
+            weighted,
+            weights_on,
+            max_edge_weight,
+            max_node_weight):
+  """Replacement-paths search timer on the <i>th graph and for all the <modes>.
+
+  It is important that upon function exit, the graphs are garbage collected.
+  """
+
+  adj_list, G = graph_generator.random_graph(
+    n=n,
+    weighted=weighted,
+    weights_on=weights_on,
+    max_edge_weight=max_edge_weight,
+    max_node_weight=max_node_weight,
+    random_seed=i
+  )
+
+  init_config = {
+    "adj_list": adj_list,
+    "adj_list_reverse": graph_generator.adj_list_reversed(adj_list),
+    "source": 1,
+    "sink": n
+  }
+
+  for k, v in modes.items():
+    mode = {
+      "bidirectional": v[0],
+      "parallel": v[1],
+      "dynamic": v[2],
+      "failing": failing,
+      "online": online,
+      "verbose": 0
+    }
+    start = timer()
+    paths = core.replacement_paths(mode, init_config)
+    end = timer()
+    times[k].append(end - start)
+
+  adj_list.clear()
+  init_config["adj_list_reverse"].clear()
+  return times
+
+
 @click.command()
 @click.argument('n', type=click.INT)
 @click.option('-s', "--increase-step", default=25, show_default=True,
@@ -58,7 +108,7 @@ def main(n,
          online,
          k):
 
-  mode = {
+  modes = {
     # 'reference': [False, False, False],
     # 'parallel': [False, True, False],
     'bidirectional': [True, False, False],
@@ -67,10 +117,10 @@ def main(n,
     # 'dynamic, bidirectional & parallel': [True, True, True]
   }
 
-  times = {k: [] for k, v in mode.items()}
+  times = {k: [] for k, v in modes.items()}
 
   times_mean = {k: [0. for _ in range(graph_increases)]
-                for k, v in mode.items()}
+                for k, v in modes.items()}
   n_values = [n + i * increase_step for i in range(graph_increases)]
 
   for j in range(graph_increases):
@@ -78,39 +128,27 @@ def main(n,
     for i in range(graphs_per_step):
       print(f"  {i + 1}/{graphs_per_step}")
 
-      adj_list, G = graph_generator.random_graph(
-        n=n,
-        weighted=weighted,
-        weights_on=weights_on,
-        max_edge_weight=max_edge_weight,
-        max_node_weight=max_node_weight,
-        random_seed=i
-      )
+      times = measure(n,
+                      i,
+                      modes,
+                      times,
+                      failing,
+                      online,
+                      weighted,
+                      weights_on,
+                      max_edge_weight,
+                      max_node_weight)
+      gc.collect()
 
-      for k, v in mode.items():
-        start = timer()
-        paths = core.replacement_paths(adj_list=adj_list,
-                                       n=n,
-                                       source=1,
-                                       sink=n,
-                                       failing=failing,
-                                       bidirectional=v[0],
-                                       parallel=v[1],
-                                       dynamic=v[2],
-                                       online=online)
-        end = timer()
-        times[k].append(end - start)
-
-        gc.collect()
-
-    for k, v in mode.items():
+    for k, v in modes.items():
       times_mean[k][j] = sum(times[k]) / graphs_per_step
       del times[k][:]
+
     n += increase_step
 
   colors = iter(['b', 'm', 'g', 'k', 'c', 'y'])
   fig, ax = plt.subplots(figsize=(10.03, 6.2), dpi=200)
-  for k, v in mode.items():
+  for k, v in modes.items():
     color = next(colors)
     ax.plot(n_values, times_mean[k], color=color, label=k)
   ax.set_xlabel("graph size (nodes)")
