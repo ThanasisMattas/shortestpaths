b697f857ee375182ec58c66ac10da4821dfc26d7
Thanasis Mattas <atmattas@physics.auth.gr>
2021-06-07 01:00:37 +0300

    test: test_read_graphs

--- shortestpaths/shortestpaths_test.py
+++ shortestpaths/shortestpaths_test.py
@@ -110,3 +110,13 @@ class TestIO():
           assert entry[0] == "set()"
         else:
           assert ast.literal_eval(", ".join(entry)) == self.adj_list[i - 1]
+
+  @pytest.mark.parametrize("num_graphs", [1, 2, 4])
+  def test_read_graphs(self, num_graphs):
+    for _ in range(num_graphs):
+      io.append_graph_to_csv(self.csvfile, self.adj_list)
+    graph_counter = 0
+    for adj in io.read_graphs(self.csvfile):
+      graph_counter += 1
+      assert adj == self.adj_list
+    assert graph_counter == num_graphs
