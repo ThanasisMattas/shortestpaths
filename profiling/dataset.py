ccc6d8cb7a0fc98201714dfa06b688237445124b
Thanasis Mattas <atmattas@physics.auth.gr>
2021-06-07 20:14:13 +0300

    script: display graph counter @ dataset.py

--- profiling/dataset.py
+++ profiling/dataset.py
@@ -15,9 +15,9 @@ from shortestpaths import graph_generator, io
 @click.command()
 @click.option('-f', "--filename", type=click.INT, default=None, show_default=True)
 @click.option("--clear", is_flag=True, default=True, prompt="Delete dataset with the same name?")
-@click.option('-n', "--initial-order", default=500, show_default=True,
+@click.option('-n', "--initial-order", default=50, show_default=True,
               help="Initial graph order")
-@click.option('-s', "--step", default=250, show_default=True,
+@click.option('-s', "--step", default=25, show_default=True,
               help="graph order increase step")
 @click.option('-i', "--increases", default=31, show_default=True)
 @click.option('-g', "--graphs-per-step", default=10, show_default=True)
@@ -45,14 +45,21 @@ def dataset(filename,
     if os.path.isfile(filename):
       os.remove(filename)
 
+  graph_counter = 0
   for n in range(initial_order, initial_order + step * increases, step):
     for p in range(1, 11):
+      graph_counter += 1
       for g in range(1, graphs_per_step + 1):
+        print((f"Graph: {graph_counter:>3}/{num_graphs // graphs_per_step}"
+               f"   Instance: {g:>2}/{graphs_per_step}"
+               f"   nodes: {n:>4}   p\u2080: {p / 10:.1f}"),
+              end='\r')
         adj_list, _ = graph_generator.random_graph(n=n,
                                                    p_0=p / 10,
                                                    random_seed=g,
                                                    **kwargs)
         io.append_graph_to_csv(filename, adj_list)
+  print()
 
 
 if __name__ == '__main__':
