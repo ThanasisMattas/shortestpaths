a3153516e48219f43aff48cf48aac122174ab479
Thanasis Mattas <atmattas@physics.auth.gr>
2024-01-14 14:09:16 +0100

    vendor: v1.1.3

--- shortestpaths/__init__.py
+++ shortestpaths/__init__.py
@@ -17,7 +17,7 @@ from shortestpaths.graph import random_graph, adj_list_reversed  # noqa: F401
 
 
 __name__ = 'shortestpaths'
-__version__ = '1.1.2'
+__version__ = '1.1.3'
 __author__ = 'Athanasios Mattas'
 __author_email__ = 'thanasismatt@gmail.gr'
 __description__ = "Bidirectional replacement paths and k-shortest paths search with dynamic programming"
